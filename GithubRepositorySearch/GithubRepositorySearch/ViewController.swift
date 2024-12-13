ccf05aeb6990ffd9ff222900d510383c3035daa2
Hyosung-finda <hyosung@finda.co.kr>
2024-12-13 11:35:12 +0900

    initialized project

---
+++ GithubRepositorySearch/GithubRepositorySearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubRepositorySearch
+//
+//  Created by 김효성 on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubRepositorySearch/GithubRepositorySearchTests/GithubRepositorySearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositorySearchTests.swift
+//  GithubRepositorySearchTests
+//
+//  Created by 김효성 on 12/13/24.
+//
+
+import Testing
+@testable import GithubRepositorySearch
+
+struct GithubRepositorySearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
