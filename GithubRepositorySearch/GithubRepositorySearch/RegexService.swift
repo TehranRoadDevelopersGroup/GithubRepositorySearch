ba32918979b781f700b0bce1f4cc1260e3a12512
Hyosung-finda <hyosung@finda.co.kr>
2024-12-22 02:13:40 +0900

    RegexService 에 TODO 추가

--- GithubRepositorySearch/GithubRepositorySearch/RegexService.swift
+++ GithubRepositorySearch/GithubRepositorySearch/RegexService.swift
@@ -5,6 +5,7 @@
 //  Created by 김효성 on 12/21/24.
 //
 
+// TODO: - 지금은 RegexService 가 wholeMatch(of:) 만 사용할 수 있도록 설계되어있음. wholeMatch(in:), prefixMatch 등과 같은 다른 정규식 검사가 필요할때 사용할 수 있도록 인터페이스를 수정해야함. 제네럴한 인터페이스로 변경하고 다른 정규식 검사 인터페이스가 제네럴한 인터페이스를 상속받도록 할 것.
 public protocol RegexService: Sendable {
   /**
    `input String` 이 `pattern Regex` 와 일치하는 경우 ` String?` 을 반환합니다.
