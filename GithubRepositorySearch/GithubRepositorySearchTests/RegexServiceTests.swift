3909c5af1f084b7fd6040249b6b163011d10960a
Hyosung-finda <hyosung@finda.co.kr>
2024-12-22 01:31:44 +0900

    RegexService, RegexHelper Test 작성

---
+++ GithubRepositorySearch/GithubRepositorySearchTests/RegexServiceTests.swift
@@ -0,0 +1,74 @@
+//
+//  RegexServiceTests.swift
+//  GithubRepositorySearchTests
+//
+//  Created by 김효성 on 12/22/24.
+//
+
+import Testing
+@testable import GithubRepositorySearch
+
+struct RegexServiceTests {
+  private let regexService: RegexService = RegexHelper()
+  private let searchQueryPattern: some RegexComponent<Substring> = #/^[a-zA-Z0-9가-힣]+$/#
+  
+  @Test func testWholeMatchWhenKoreanThenInputVaild() async throws {
+    let input = "안녕하세요"
+    
+    let matched = regexService
+      .wholeMatch(
+        input: input,
+        of: searchQueryPattern
+      )
+    
+    #expect(input == matched)
+  }
+  
+  @Test func testWholeMatchWhenEnglishThenInputVaild() async throws {
+    let input = "abcdef"
+    
+    let matched = regexService
+      .wholeMatch(
+        input: input,
+        of: searchQueryPattern
+      )
+    
+    #expect(input == matched)
+  }
+  
+  @Test func testWholeMatchWhenNumberThenInputVaild() async throws {
+    let input = "123123"
+    
+    let matched = regexService
+      .wholeMatch(
+        input: input,
+        of: searchQueryPattern
+      )
+    
+    #expect(input == matched)
+  }
+  
+  @Test func testWholeMatchWhenMixThenInputVaild() async throws {
+    let input = "123123abcdef안녕하세요"
+    
+    let matched = regexService
+      .wholeMatch(
+        input: input,
+        of: searchQueryPattern
+      )
+    
+    #expect(input == matched)
+  }
+  
+  @Test func testWholeMatchWhenKoreanThenInputInvaild() async throws {
+    let input = "안녕ㅎㅏ세요"
+    
+    let matched = regexService
+      .wholeMatch(
+        input: input,
+        of: searchQueryPattern
+      )
+    
+    #expect(matched == nil)
+  }
+}
